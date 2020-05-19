241c2f532e41010743f9cf843e0be9bc39b929f5
KING-PC\King <ryan13king@gmail.com>
2020-05-18 18:44:06 -0700

    Added Buttons
    added buttons to HTML

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -1,2 +1,29 @@
 /*eslint-env browser*/
+window.document.addEventListener("DOMContentLoaded", init);
+function init() {
+
+    let BtnName = document.getElementById('name');
+    let BtnDeposit = document.getElementById('deposit');
+    let BtnWithdrawal = document.getElementById('withdrawal');
+
+    BtnDeposit.addEventListener('click', () => {
+        deposit = window.prompt('Enter your name');
+        window.console.log(name);
+    });
+
+    Btn.addEventListener('click', () => {
+        name = window.prompt('Enter your name');
+        window.console.log(name);
+    });
+
+    BtnName.addEventListener('click', () => {
+        name = window.prompt('Enter your name');
+        window.console.log(name);
+    });
+
+
+
+};
+
+
 
