db41630e5ea1a97ef1370c02ffeb7fe022931146
KING-PC\King <ryan13king@gmail.com>
2020-05-18 19:44:16 -0700

    updated

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -2,25 +2,39 @@
 window.document.addEventListener("DOMContentLoaded", init);
 function init() {
 
-    let BtnName = document.getElementById('name');
-    let BtnDeposit = document.getElementById('deposit');
-    let BtnWithdrawal = document.getElementById('withdrawal');
 
-    BtnDeposit.addEventListener('click', () => {
-        deposit = window.prompt('Enter your name');
+    let btnName = document.getElementById('name');
+    let btnDeposit = document.getElementById('deposit');
+    let btnWithdrawal = document.getElementById('withdrawal');
+
+    btnName.addEventListener('click', () => {
+        name = window.prompt('Enter your name:');
+        document.getElementById('acctName').innerHTML = name;
+        document.getElementById('acctBal').innerHTML = 0;
         window.console.log(name);
     });
 
-    Btn.addEventListener('click', () => {
-        name = window.prompt('Enter your name');
-        window.console.log(name);
+    btnDeposit.addEventListener('click', () => {
+        deposit = window.prompt('Enter amount to deposit:');
+        bankAccount(amount);
+        window.console.log(deposit);
     });
 
-    BtnName.addEventListener('click', () => {
-        name = window.prompt('Enter your name');
-        window.console.log(name);
+    btnWithdrawal.addEventListener('click', () => {
+        withdrawal = window.prompt('Enter amount to withdraw:');
+        bankAccount();
+        window.console.log(withdrawal);
     });
 
+    function bankAccount(){
+        let balance;
+        // let owner;
+
+        balance = document.getElementById('acctBal').innerHTML;
+        // owner = ownerName;
+
+    
+    };
 
 
 };
